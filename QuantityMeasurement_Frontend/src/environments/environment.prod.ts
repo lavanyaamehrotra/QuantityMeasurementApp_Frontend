df938965268cbe3b7b01ddb0460e3ecf545aac44
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2026-04-07 00:17:17 +0530

    [Lavanya Mehrotra] .refactored the code

--- QuantityMeasurement_Frontend/src/environments/environment.prod.ts
+++ QuantityMeasurement_Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBase: '/api/v1',
+  apiBase: 'https://quantitymeasurementapp-bkle.onrender.com/api/v1',
   googleClientId: '1086320009938-trpein80mt33nam412hvbe4jsng9fbl3.apps.googleusercontent.com'
 };
